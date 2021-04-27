5728a7403f07226635c0ab3fde14c6975876e57e
James Camilleri <james@james.com.mt>
2021-04-27 01:02:23 +0100

    Add build plugins

---
+++ build-plugins/create-gallery.cjs
@@ -0,0 +1,77 @@
+const camelCase = require('camel-case').camelCase
+const stringify = require('javascript-stringify').stringify
+const visit = require('unist-util-visit')
+
+function isGalleryNode (node) {
+  return node.type === 'list' &&
+    node.children.length === 1 &&
+    node.children[0].children[0].children[0].value.toLowerCase() === 'gallery'
+}
+
+function extractImages (list) {
+  return list
+    .children // list items
+    .map(({ children }) => children[0]) // paragraphs
+    .map(({ children }) => children) // images + dividers
+    .map(row => row.filter(({ type }) => type === 'image'))
+}
+
+function unquoteSrc (string) {
+  return string.replace(/src:'(.*?)'/g, (_, match) => `src:${match}`)
+}
+
+function generateGalleryComponent (images) {
+  const galleryProps = images.map(
+    row => row.map(image => ({ src: image.url, alt: image.alt }))
+  )
+
+  const stringifiedProps = unquoteSrc(stringify(galleryProps))
+  return `<Gallery media={${stringifiedProps}} />`
+}
+
+function importLocalImages (images, tree) {
+  const imports = []
+
+  images.forEach(row => {
+    row.forEach(image => {
+      const { url } = image
+      if (!url.includes('./')) return
+
+      const name = camelCase(url)
+
+      image.url = name
+      imports.push(`import ${name} from '${url}'`)
+    })
+  })
+
+  // TODO: Handle existing <script> tags, extract Gallery import.
+  const scriptTag = '<script>\n'
+    + imports.join('\n')
+    + '\nimport Gallery from "$lib/Gallery/index.svelte"\n'
+    + '</script>'
+
+  tree.children.unshift({
+    type: 'html',
+    value: scriptTag
+  })
+}
+
+function createGallery () {
+  return function transformer (tree, file) {
+    visit(tree, 'list', gallery)
+
+    function gallery (node) {
+      if (!isGalleryNode(node)) return
+
+      const images = extractImages(node.children[0].children[1])
+      importLocalImages(images, tree)
+
+      const gallery = generateGalleryComponent(images)
+
+      node.type = 'html'
+      node.value = gallery
+    }
+  };
+}
+
+module.exports = createGallery
---
+++ build-plugins/wrap-portfolio-content.cjs
@@ -0,0 +1,29 @@
+function wrapPortfolioContent () {
+  return function transformer (tree, file) {
+    const rawNodes = tree.children
+      .filter(node => node.type === 'raw' && !['', '\n'].includes(node.value))
+
+    const context = rawNodes
+      .filter(node => node.value.includes('<script context'))[0]
+    const script = rawNodes
+      .filter(node => node.value.includes('<script>'))[0]
+    const gallery = rawNodes
+      .filter(node => node.value.includes('<Gallery'))[0]
+
+    const content = tree.children.filter(node => node.type !== 'raw')
+    const wrappedContent = {
+      type: 'element',
+      tagName: 'div',
+      properties: { class: 'content' },
+      children: content
+    }
+
+    tree.children = []
+    if (context) tree.children.push(context)
+    if (script) tree.children.push(script)
+    if (wrappedContent) tree.children.push(wrappedContent)
+    if (gallery) tree.children.push(gallery)
+  }
+}
+
+module.exports = wrapPortfolioContent
