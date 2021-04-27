11aa4968902ced3258c01c396fd88a8531014528
James Camilleri <james@james.com.mt>
2021-04-27 02:23:35 +0100

    Create portfolio item layout

---
+++ src/routes/portfolio/$layout.svelte
@@ -0,0 +1,15 @@
+<div class="portfolio">
+  <slot />
+</div>
+
+<style lang="scss">
+  .portfolio {
+    display: grid;
+    grid-template-columns: calc(var(--column-width) * 2) 1fr;
+    gap: var(--space);
+
+    @media screen and (max-width: 70rem) {
+      grid-template-columns: 100%;
+    }
+  }
+</style>
