a9d1e87395ffdfdbbaab0e8a6dde12f0a08d7521
James Camilleri <james@james.com.mt>
2021-04-28 18:19:42 +0100

    Add puzzle posters portfolio item

---
+++ src/routes/portfolio/puzzle-posters/_assets/Answers.svelte
@@ -0,0 +1,23 @@
+<div>Select the empty area below to reveal the hidden text:</div>
+<textarea spellcheck="false">POSTER 1:
+While this looks like a simple repeating pattern at first, it's actually a little more complex than that. The black dots are actually abstracted representations of braille letters, when decoded spell out EEFGG FEDCC DEEDF EEFGG FEDCC – the opening notes to Beethoven's Ode to Joy. Completing the melody reveals the final 5 pattern components.
+
+POSTER 2:
+The mechanics of this puzzle are a little simpler than the previous, but a lot more legwork is required to get to the final answer. The grid of letters coupled with the coordinates in the blank spaces below can be used to formulate a set of Roman numerals, which when converted to standard digits read 35.895989,14.510712 – the coordinates of a plaque in the city of Valletta, Malta. For the second part of the puzzle, the coordinates pinpoint words on the plaque that are used to fill the crossword-like construction and form the central character sequence. Finally, the (3, 2, 6) → (12, 1, 7) coordinates points to two individual letters on the plaque which are used as the key for a Caeser cipher. When decoding the previously obtainer character sequence with the Caeser substitution, the phrase "CARPEDIEM" is revealed.
+</textarea>
+
+<style>
+  textarea {
+    width: 100%;
+    height: 400px;
+    padding: 0.5rem;
+    border: solid var(--black);
+    background: none;
+    resize: none;
+    font-family: circe-slab-a;
+    font-size: 1rem;
+    font-weight: 500;
+    color: var(--background);
+
+  }
+</style>
---
+++ src/routes/portfolio/puzzle-posters/index.md
@@ -0,0 +1,14 @@
+---
+title: puzzle posters
+tags: [graphic design, puzzles]
+description: a pair of promotional posters incorporating complex puzzles into the design
+---
+
+While these two posters are hardly groundbreaking designs, the fact that they directly engage the viewer by giving them a puzzle to solve makes them a bit different from the usual promotion. Answers are at the bottom of the page if your curiosity gets the better of you.
+
+(The second one requires you to actually visit somewhere, so unless you're in Malta I'm afraid you won't be able to solve that fully.)
+
+- GALLERY
+  - ![Puzzle poster](./_assets/poster-1.png)
+  - ![Puzzle poster](./_assets/poster-2.png)
+  - ![:component:](./_assets/Answers.svelte)
