da2eca53cd2584d666b51def95a7d1f4508107f3
James Camilleri <james@james.com.mt>
2021-04-28 14:56:27 +0100

    Fix tag wrapping

--- src/lib/components/TagList.svelte
+++ src/lib/components/TagList.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <style lang='scss'>
+  div {
+    display: flex;
+    flex-wrap: wrap;
+  }
   span {
+    margin-bottom: 0.3rem;
     padding: 0.2rem 0.3rem;
     font-size: 0.6rem;
     color: var(--background);
